e993ec8d162f77f875997bc89c72c10d06f474c3
JVViacrusis <justin.viacrusis21p@gmail.com>
2024-11-12 02:04:34 -0800

    commented out data randomization for now

--- dim-reduction/data_preprocessing.py
+++ dim-reduction/data_preprocessing.py
@@ -4,7 +4,7 @@ from sklearn.utils import shuffle
 def load_and_preprocess_data(config):
     """Load and preprocess the dataset based on the configuration."""
     data = pd.read_csv(config['dataset']['path'])
-    data = shuffle(data)
+    # data = shuffle(data)
     if 'drop_columns' in config['dataset']:
         data = data.drop(columns=config['dataset']['drop_columns'])
     X = data.drop(columns=[config['dataset']['target_column']])
